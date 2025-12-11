248dddad70f2f7bedcf0a8cbcb18ffaff8b4f79c
composable-tu <composable@foxmail.com>
2025-12-11 15:38:17 +0800

    chore(deps): 更新依赖包到最新版本
    - 将 lucide-vue-next 从 0.556.0 更新至 0.59.0
    - 将 nuxt 从 4.2.1 更新至 4.2.2
    - 将 shadcn-nuxt 从 2.4.2 更新至 2.4.3
    - 将 @types/node 从 24.10.1 更新至 25.0.0
    - 将 oxlint-tsgolint 从 0.8.4 更新至 0.8.5

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,22 +1,14 @@
 <script lang="ts" setup>
 import LoginForm from "@/components/LoginForm.vue"
-import {ref} from 'vue'
 
 definePageMeta({
   layout: 'other',
 })
-
-const logoSrc = ref('/LogoColorBlack.png')
-
 </script>
 
 <template>
   <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
     <div class="flex w-full max-w-sm flex-col gap-6">
-      <div class="flex items-center gap-2 self-center font-medium">
-        <div class="flex h-16 w-48 items-center justify-center bg-contain bg-no-repeat bg-center">
-        </div>
-      </div>
       <LoginForm/>
     </div>
   </div>
